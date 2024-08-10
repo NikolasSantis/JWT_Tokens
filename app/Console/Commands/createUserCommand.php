bfdc8e2e022db1ba533a09ff7030af95584c135c
NikolasSantis <nicolastigosan@gmail.com>
2024-08-10 13:50:09 -0300

    feat: getting-command-args-and-validate

--- app/Console/Commands/createUserCommand.php
+++ app/Console/Commands/createUserCommand.php
@@ -27,6 +27,38 @@ class createUserCommand extends Command
      */
     public function handle()
     {
+        $data = [
+            'name' => $this->argument('name'),
+            'email' => $this->argument('email'),
+            'password' => $this->argument('password')
+        ];
 
+        $messages = [
+            'name.required' => 'Um :attribute de usuário é obrigatório.',
+            'name.max' => 'O :attribute de usuário é muito grande',
+            'email.email' => 'O :attribute informado não é um endereço email válido',
+            'email.unique' => 'O :attribute já está sendo usando por outro usuário',
+            'passowrd.required' => 'É necessário inserir uma :attribute',
+            'password.min' => 'A :attribute é muito curta'
+        ];
+
+        $attributes = [
+            'name' => 'nome', 
+            'email' => 'e-mail',
+            'password' => 'senha'
+        ];
+
+        $validator = Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:5'
+        ], $messages, $attributes);
+
+        
+        if ($validator->fails()) {
+            dd($validator->errors());
+        };
+
+        
     }
 }
