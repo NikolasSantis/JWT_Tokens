3d9cb64a352e62b0fdb3174155ea0bc723de28ba
NikolasSantis <nicolastigosan@gmail.com>
2024-09-14 14:12:44 -0300

    feat: add-update-user-logic-command

--- app/Console/Commands/updateUserCommand.php
+++ app/Console/Commands/updateUserCommand.php
@@ -3,6 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\Console\Question\Question;
 
 class updateUserCommand extends Command
 {
@@ -11,7 +16,7 @@ class updateUserCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'app:update_user_command';
+    protected $signature = 'app:update_user_command {--id= : User id} {--email= : User email}';
 
     /**
      * The console command description.
@@ -25,6 +30,59 @@ class updateUserCommand extends Command
      */
     public function handle()
     {
-        //
+        $userId = $this->option('id');
+        $userEmail = $this->option('email');
+
+        $user = User::where('id', $userId)
+            ->orWhere('email', $userEmail)
+            ->get()[0];
+
+        echo "\n Usuário $user->name encontrado. Inserir novos valores: ";
+        $updateValues = [
+            'name' => $this->ask('Novo nome de usuário'),
+            'email' => $this->ask('Novo email de usuário'),
+            'password' => $this->secret('Nova senha de usuário'),
+        ];
+
+        if (empty(array_filter($updateValues))) {
+            dd('Empty array to update. Try again with some value');
+        }
+
+        $validator = Validator::make($updateValues, [
+            'name' => 'string|max:255|nullable',
+            'email' => 'email|unique:users|nullable',
+            'password' => 'string|min:5|nullable'
+        ]);
+
+        if ($validator->fails()) {
+            dd($validator->errors()); 
+        }
+
+        echo "Dados para o usuário $user->name com Id $user->id";
+        foreach($updateValues as $label => $value) {
+            if ($label == 'password') {
+                $value = str_repeat('*', strlen($value));
+            }
+            echo "\n $label => " . ($value ?? 'null');
+        }
+
+        if ($this->ask("\n\nConfirma os valores de atualização[y/n]") == 'n') {
+            echo 'Valores não atualizados.';
+            return;
+        }
+
+        $updateValues['password'] = !empty($updateValues['password']) ?
+            password_hash($updateValues['password'], PASSWORD_BCRYPT, ['cost' => 12]) :
+            null;
+
+        try {
+            $affectedRows = $user::where('id', $userId)->update(
+                array_filter($updateValues)
+            );
+        } catch (\Exception $e) {
+            return $e;
+        }
+        echo "$affectedRows Rows Affected";
+        return true;
     }
 }
